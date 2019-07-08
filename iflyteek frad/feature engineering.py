0000acd8b75bbee1bbfdc30e51df2063620841df
ben <389369676@qq.com>
2019-07-08 23:09:23 +0800

    2019/7/8

--- iflyteek frad/feature engineering.py
+++ iflyteek frad/feature engineering.py
@@ -4,4 +4,17 @@ Created on Mon Jul  8 22:28:29 2019
 
 @author: Administrator
 """
+import pandas as pd
+
+orldata=pd.read_csv('D://frad_train.csv')
+
+orldata.head()
+
+feature=orldata.columns.values.tolist()
+
+orldata.dtypes.value_counts()
+
+sample=orldata.iloc[0:100,:]
+
+t2=orldata.select_dtypes('object').astype(str)
 
