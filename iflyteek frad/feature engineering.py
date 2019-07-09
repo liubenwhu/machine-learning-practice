b464d8a2bee5ccdd78d3461b21f52db1c3d647a8
ben <389369676@qq.com>
2019-07-09 16:35:42 +0800

    2019/7/9/16:35

--- iflyteek frad/feature engineering.py
+++ iflyteek frad/feature engineering.py
@@ -14,8 +14,37 @@ feature=orldata.columns.values.tolist()
 
 orldata.dtypes.value_counts()
 
-sample=orldata.iloc[0:100,:1]
+sample=orldata.iloc[0:100,:]
 
 t1=orldata.select_dtypes('object').apply(pd.Series.nunique, axis = 0)
 t2=orldata.select_dtypes('int64').apply(pd.Series.nunique, axis = 0)
 
+def click_counting(sample, bin_column):
+    clicks = pd.Series(sample[sample['label'] > 0][bin_column].value_counts(), name='label-yes')
+    no_clicks = pd.Series(sample[sample['label'] < 1][bin_column].value_counts(), name='label-no')
+    
+    counts = pd.DataFrame([clicks,no_clicks]).T.fillna('0')
+    counts['total'] = counts['label-yes'].astype('int64') + counts['label-no'].astype('int64')
+    
+    return counts
+
+def bin_counting(counts):
+    counts['N+'] = counts['label-yes'].astype('int64').divide(counts['total'].astype('int64'))
+    counts['N-'] = counts['label-no'].astype('int64').divide(counts['total'].astype('int64'))
+    counts['log_N+'] = counts['N+'].divide(counts['N-'])
+
+#    If we wanted to only return bin-counting properties, we would filter here
+    bin_counts = counts.filter(items= ['N+', 'N-', 'log_N+'])
+    return counts, bin_counts
+
+
+# bin counts example: device_id
+bin_column = 'pkgname'
+device_clicks = click_counting(sample.filter(items= [bin_column, 'label']), bin_column)
+device_all, device_bin_counts = bin_counting(device_clicks)
+
+#ind=device_all.index.values.tolist()
+#ind=pd.DataFrame(ind,columns=['pkgname'])
+device_all.rename(columns={'index': 'pkgname'}, inplace=True) 
+
+result = pd.merge(sample, device_all, how='left', on='pkgname')
